dc7551f3a6cc618d6b7f954bf2349af92e0c4f0c
Lucas Amorim <luucasads@hotmail.com>
2019-02-07 21:45:57 -0200

    Primeiro commit

---
+++ src/app/logo/logo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'cd-logo',
+    templateUrl: 'logo.component.html'
+})
+export class LogoComponent {
+     url = 'https://static.vecteezy.com/system/resources/previews/000/238/859/large_2x/dog-family-vector-illustration.jpg';
+    description: 'Logo Canil';
+
+}
